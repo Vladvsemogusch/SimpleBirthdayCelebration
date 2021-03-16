a6e65516ef006cb39a70fc81f7ec74abe7c9836b
Vladyslav <anisimov.vlad@gmail.com>
2021-03-16 22:24:25 +0200

    Birthday screen - add init fields

--- app/src/main/java/cc/anisimov/vlad/letscelebrate/domain/viewmodel/BirthdayViewModel.kt
+++ app/src/main/java/cc/anisimov/vlad/letscelebrate/domain/viewmodel/BirthdayViewModel.kt
@@ -1,16 +1,25 @@
 package cc.anisimov.vlad.letscelebrate.domain.viewmodel
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import cc.anisimov.vlad.letscelebrate.domain.model.BirthdayData
+import cc.anisimov.vlad.letscelebrate.domain.model.ImageData
 import cc.anisimov.vlad.letscelebrate.util.SingleLiveEvent
+import java.util.*
 
 class BirthdayViewModel @ViewModelInject constructor() : ViewModel() {
 
     val oError = SingleLiveEvent<String?>()
+    val oName = MutableLiveData("")
+    val oImageData = MutableLiveData<ImageData?>()
+    private lateinit var birthdayDate: Date
+
 
     fun initWith(birthdayData: BirthdayData) {
-
+        oName.value = birthdayData.name
+        birthdayDate = birthdayData.birthdayDate
+        oImageData.value = birthdayData.imageData
     }
 
 }
