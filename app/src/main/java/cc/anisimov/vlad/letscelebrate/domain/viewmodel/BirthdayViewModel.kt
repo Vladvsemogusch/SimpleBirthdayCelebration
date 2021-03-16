f6ac79f69577b2e30f056bc1c946edb1dce34347
Vladyslav <anisimov.vlad@gmail.com>
2021-03-16 22:01:47 +0200

    Birthday screen - add error handling

--- app/src/main/java/cc/anisimov/vlad/letscelebrate/domain/viewmodel/BirthdayViewModel.kt
+++ app/src/main/java/cc/anisimov/vlad/letscelebrate/domain/viewmodel/BirthdayViewModel.kt
@@ -3,10 +3,14 @@ package cc.anisimov.vlad.letscelebrate.domain.viewmodel
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import cc.anisimov.vlad.letscelebrate.domain.model.BirthdayData
+import cc.anisimov.vlad.letscelebrate.util.SingleLiveEvent
 
 class BirthdayViewModel @ViewModelInject constructor() : ViewModel() {
 
+    val oError = SingleLiveEvent<String?>()
+
     fun initWith(birthdayData: BirthdayData) {
+
     }
 
 }
--- app/src/main/java/cc/anisimov/vlad/letscelebrate/ui/fragment/BirthdayFragment.kt
+++ app/src/main/java/cc/anisimov/vlad/letscelebrate/ui/fragment/BirthdayFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.navArgs
 import cc.anisimov.vlad.letscelebrate.R
 import cc.anisimov.vlad.letscelebrate.domain.viewmodel.BirthdayViewModel
+import cc.anisimov.vlad.letscelebrate.ui.common.setupErrorHandling
 
 class BirthdayFragment : Fragment() {
     private val viewModel: BirthdayViewModel by activityViewModels()
@@ -24,6 +25,7 @@ class BirthdayFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel.initWith(birthdayFragmentArgs.birthdayData)
+        setupErrorHandling(viewModel.oError)
     }
 
 }
