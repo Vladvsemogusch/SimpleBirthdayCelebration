999157fbd4be05582d8b5b1ced0abc66ec968fa0
Vlad Anisimov <anisimov@dbosoft.com.ua>
2021-03-17 13:20:41 +0200

    Add age period title

--- app/src/main/java/cc/anisimov/vlad/letscelebrate/ui/fragment/BirthdayFragment.kt
+++ app/src/main/java/cc/anisimov/vlad/letscelebrate/ui/fragment/BirthdayFragment.kt
@@ -36,26 +36,33 @@ class BirthdayFragment : Fragment() {
             if (age == null) {
                 return@observe
             }
+            val periodName: String
             val imageResId = if (age.years == 0 || (age.years == 1 && age.months == 0)) {
-                when (age.months) {
-                    0 -> R.drawable.n0
-                    1 -> R.drawable.n1
-                    2 -> R.drawable.n2
-                    3 -> R.drawable.n3
-                    4 -> R.drawable.n4
-                    5 -> R.drawable.n5
-                    6 -> R.drawable.n6
-                    7 -> R.drawable.n7
-                    8 -> R.drawable.n8
-                    9 -> R.drawable.n9
-                    10 -> R.drawable.n10
-                    11 -> R.drawable.n11
-                    12 -> R.drawable.n12
-                    else -> throw IllegalArgumentException("Month ${age.months} doesn't exist")
+                periodName = resources.getQuantityString(R.plurals.month,age.months)
+                if (age.years==1){
+                    R.drawable.n12
+                }else {
+                    when (age.months) {
+                        0 -> R.drawable.n0
+                        1 -> R.drawable.n1
+                        2 -> R.drawable.n2
+                        3 -> R.drawable.n3
+                        4 -> R.drawable.n4
+                        5 -> R.drawable.n5
+                        6 -> R.drawable.n6
+                        7 -> R.drawable.n7
+                        8 -> R.drawable.n8
+                        9 -> R.drawable.n9
+                        10 -> R.drawable.n10
+                        11 -> R.drawable.n11
+                        else -> throw IllegalArgumentException("Month ${age.months} doesn't exist")
+                    }
                 }
             } else if (age.years == 1 && age.months in 6..11) {
+                periodName = resources.getQuantityString(R.plurals.year,age.years)
                 R.drawable.n1_half
             } else if (age.years < 12) {
+                periodName = resources.getQuantityString(R.plurals.year,age.years)
                 when (age.years) {
                     0 -> R.drawable.n0
                     1 -> R.drawable.n1
@@ -75,6 +82,7 @@ class BirthdayFragment : Fragment() {
             } else {
                 throw IllegalArgumentException("Ages above 12 are not supported")
             }
+            tvPeriodName.text = getString(R.string.period_old, periodName)
             ivAge.setImageResource(imageResId)
         }
     }
